2558dfe5b7de53f0aa1f9a81655ea47b1170d4c4
SIVASUBRAMANIAM L <141582345+shivas1516@users.noreply.github.com>
2023-10-18 22:42:49 +0530

    Add files via upload

---
+++ AI_phase3.py
@@ -0,0 +1,42 @@
+# Chatbot Project Submission - Phase 3
+
+## Project Information
+
+- **Project Name**: chatbot in Python
+- **Phase**: 3 - Development Part 1
+
+
+## Introduction
+
+This document serves as the official submission for Phase 3 of the chatbot project. This phase involves the initial development steps, including setting up the development environment, loading and preprocessing the dataset, and implementing basic user interactions.
+
+## Phase 3 Tasks
+
+### Step 1: Setting Up the Development Environment
+
+- [ ] Created a virtual environment for the project.
+- [ ] Installed required libraries, including Flask and Transformers.
+- [ ] Initialized a Git repository and created a GitHub repository for the project.
+
+### Step 2: Loading and Preprocessing the Dataset
+
+- [ ] Downloaded the dataset from the provided Kaggle link.
+- [ ] Preprocessed the data as necessary to make it suitable for the chatbot's training.
+
+### Step 3: Basic User Interactions
+
+- [ ] Set up a basic Flask web application for user interactions.
+- [ ] Defined routes for the web application.
+- [ ] Implemented basic chatbot logic to respond to user interactions.
+- [ ] Designed a simple user interface for the web app.
+
+### Step 4: Submission
+
+- [ ] Organized project files and code according to the specified naming convention.
+- [ ] Uploaded project files, including code and dataset, to the private GitHub repository.
+- [ ] Granted access to faculty evaluators and the industry evaluator by adding their email addresses as collaborators on the GitHub repository.
+
+## Conclusion
+
+This phase marks the beginning of the development journey for the chatbot project. I have successfully completed the tasks mentioned above and am prepared for the next phase, where I will enhance the chatbot's capabilities and improve its performance.
+
